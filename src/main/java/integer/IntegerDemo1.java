561a92dd87a2001c09925bc7bb29bd17213522d5
MarioHMY <724912105@qq.com>
2021-10-18 16:33:11 +0800

    第二天

---
+++ src/main/java/integer/IntegerDemo1.java
@@ -0,0 +1,23 @@
+package integer;
+
+/**
+ * 包装类
+ * 包装类是为了解决基本类型不能直接参与面向对象开发（没有面向对象的三大特征），因此，
+ * 包装类可以让基本类型以“对象”的形式存在
+ */
+public class IntegerDemo1 {
+    public static void main(String[] args) {
+        int i = 123;
+        //java不推荐用new创建包装类
+        Integer i1 = new Integer(i);
+        Integer i2 = new Integer(i);
+        System.out.println(i1==i2);
+        System.out.println(i1.equals(i2));
+
+        //重用1字节之内的整数对象（-128-127）
+        Integer i3 = Integer.valueOf(i);
+        Integer i4 = Integer.valueOf(i);
+        System.out.println(i3==i4);
+        System.out.println(i3.equals(i4));
+    }
+}
