f3b9a4eb67aced56668bbb820a2bbabfb4fd73ba
MarioHMY <724912105@qq.com>
2021-11-02 08:34:32 +0800

    第八天

---
+++ src/main/java/thread/SyncDemo4.java
@@ -0,0 +1,29 @@
+package thread;
+
+/**
+ * 互斥锁
+ * 当使用synchronized锁定多个不同的代码片段，并且指定的同步监视器对象相同时，
+ * 这些代码片段之间就是互斥的，即:多个线程不能同时访问这些方法。
+ */
+public class SyncDemo4 {
+    public static void main(String[] args) {
+
+    }
+}
+class Foo{
+    Thread t = Thread.currentThread();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
